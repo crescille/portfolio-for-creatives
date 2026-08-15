8ae6c3efbc5aacb6d3889c019c976352c19f29ad
crescille <crescilleb@gmail.com>
2026-08-15 13:43:39 -0700

    lots of changes and addition

--- js/cursor.js
+++ js/cursor.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const cursor = document.createElement("div");
   cursor.id = "cursor";
-  cursor.innerHTML = `<span class="more info">view</span>`;
+  cursor.innerHTML = `<span class="info">view</span>`;
   document.body.appendChild(cursor);
 
   document.addEventListener("mousemove", (e) => {
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     cursor.style.top = e.clientY + "px";
   });
 
-  document.querySelectorAll("[data-cursor-title]").forEach((el) => {
+  document.querySelectorAll("a").forEach((el) => {
     el.addEventListener("mouseenter", () => {
       cursor.classList.add("hovering");
     });
